39dd8d1371e52abff14964e6ed3b974f0362b8df
gjc9620 <992758355@qq.com>
2016-04-04 23:23:27 +0800

    best

---
+++ best.js
@@ -0,0 +1,9 @@
+function best(coll, rule){
+    return coll.reduce(function(pre, curr){
+        return rule(pre, curr)? pre : curr
+    })
+}
+
+console.log(best([44,2,4,2,1,333], function (x, y) {
+    return x > y
+}))
