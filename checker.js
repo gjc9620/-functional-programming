e727f860f0df19c41c4c32622b2fb4fe63d8e06a
gjc9620 <992758355@qq.com>
2016-04-21 23:12:32 +0800

    debug

--- checker.js
+++ checker.js
@@ -1,12 +1,28 @@
+var toArray = require("./myLib/toArray");
+
 function checker() {
-  var validators = toArray(argument);
+  var validators = toArray(arguments);
   return function(obj) {
+    // console.log(validators)
     return validators.reduce(function(errs, check) {
-      if(check[obj]){
+
+      if(check(obj)){
         return errs
       }else{
-        return errs.push(check.message)
+        errs.push(check.message)
+        return errs
       }
-    },[])
+    },[]);
   }
 }
+
+// var ch  = function (v) {
+//   var ee =  v[0] !== 1;
+//   console.log(ee)
+//
+//   return ee
+// };
+// ch.message = "not 1"
+//
+// var fu = checker(ch)
+// console.log( fu([1,22,3]) )
