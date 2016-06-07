34445be2fb7e14517d5172a213e7bd70ae54933e
gjc9620 <992758355@qq.com>
2016-06-07 09:40:12 +0800

    update

--- andify.js
+++ andify.js
@@ -10,12 +10,18 @@ function andify(){
 	return function (){
 		var args = toArray(arguments);
 		var everything = function(ps, truth){
-			if(ps.length){
+			console.log(ps);
+			if(ps.length===0){
 				return truth
 			}else {
 				return everything(args, first(ps)) && everything(rest(ps), truth);
 			}
-		}
+		};
 		return everything(preds, true)
 	}
 }
+
+var ggg = andify(v=>v===1, v=>v+1===2);
+var tt = ggg(1,2,3);
+console.log(tt);
+//why not work
