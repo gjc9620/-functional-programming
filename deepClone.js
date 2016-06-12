f8d477702c7ce10d0bb8d7c0862b3b0d23c3f52c
gjc9620 <992758355@qq.com>
2016-06-12 08:44:42 +0800

    isobject

--- deepClone.js
+++ deepClone.js
@@ -2,7 +2,18 @@
  *
  * Created by gujiacheng on 16/6/8.
  */
+var existy = require("./myLib/existy"),
+    isObject = require("./myLib/existy");
 
 function deepClone(obj){
-	//if
+	if(!existy(obj) || !isObject(obj)){
+		return obj;
+	}
+	var temp = new obj.constructor();
+	for(var key in obj){
+		temp[key] = obj[key]
+	}
+	return temp;
 }
+
+console.log(  deepClone({a:1}) )
---
+++ myLib/isObject.js
@@ -0,0 +1,4 @@
+/**
+ * Created by gujiacheng on 16/6/12.
+ */
+module.export = function(obj){ return Object.prototype.toString,apply(obj) === "[object Array]"};
