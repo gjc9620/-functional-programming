bde20ad31268ff80399c56802c34e755a264eb05
gjc9620 <992758355@qq.com>
2016-06-15 09:26:25 +0800

    update

--- Lazychain.js
+++ Lazychain.js
@@ -13,14 +13,18 @@ function Lazychain(obj){
 Lazychain.prototype.invoke = function(methodName /*, args*/){
   var args = rest(arguments);
 	this._calls.push(function(target){
-    var meth = target[methodName];
-		return meth.apply(target, args);
+		return target[methodName](...args);
 	});
 	return this;
 };
 
 Lazychain.prototype.forse = function(){
-	return this._calls.reduce(thunk=>{
-    return thunk(this._target);
-	})
+	return this._calls.reduce((value ,thunk)=>{
+    return thunk(value);
+	},this._target)
 };
+
+var l1 = new Lazychain([2,3,1]);
+l1.invoke("sort", (a, b)=>b>a )
+  .invoke("map", v=>v*2 );
+console.log(l1.forse())
